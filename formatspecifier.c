67a85d334669df637beae9738fb313e652b49c38
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2023-02-17 19:05:39 +0530

    Second commit

---
+++ formatspecifier.c
@@ -0,0 +1,13 @@
+//Using format specifier in the C lang
+#include<stdio.h>
+int main()
+{
+  int Rollnum = 21;
+  float Pi = 3.14;
+  char name = 'A';
+
+  printf("My roll number is = %d \n", Rollnum);
+  printf("Value of pi = %f \n", Pi);
+  printf("My name starts with the name of %c",name);
+  return 0;
+}
