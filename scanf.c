f19fdfbbdd2859aeafe4d7e7b84d8d42739590dc
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2023-02-18 16:32:01 +0530

    using Scanf function

---
+++ scanf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a , b , sum;
+    
+    printf("Enter the first integer... \n");
+    scanf("%d", &a);
+
+    printf("Enter the second integer...\n");
+    scanf("%d" , &b);
+
+    sum = a+b;
+    printf("Sum of 1st and 2nd integer is = %d", sum);
+
+}
